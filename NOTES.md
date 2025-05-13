2b3009fbe8cba14780a9769b46df5c0a37e30dbf
João Palmeiro <joaommpalmeiro@gmail.com>
2025-05-13 16:15:06 +0100

    Closes #4

--- NOTES.md
+++ NOTES.md
@@ -1,6 +1,18 @@
 # Notes
 
 - https://github.com/joaopalmeiro/template-python-uv-script
+- https://pypi.org/project/mlx-audio/
+- Models:
+  - https://github.com/hexgrad/kokoro
+    - https://github.com/hexgrad/kokoro/blob/2668b2e279d0f94977995230e523b0183763f30e/kokoro.js/src/voices.js#L4
+    - https://huggingface.co/hexgrad/Kokoro-82M/blob/main/VOICES.md
+  - https://github.com/kyutai-labs/moshi
+  - https://github.com/suno-ai/bark
+    - https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c
+    - https://huggingface.co/docs/transformers/model_doc/bark
+    - https://huggingface.co/spaces/suno/bark
+    - https://huggingface.co/suno/bark-small
+    - [how to set The attention mask and the pad token id ?](https://github.com/suno-ai/bark/issues/402) issue
 
 ## Commands
 
--- generate_my_name.py
+++ generate_my_name.py
@@ -1,2 +1,27 @@
+from pathlib import Path
+
+from gaveta.files import ensure_dir
+from scipy.io.wavfile import write as write_wav
+from transformers import AutoProcessor, BarkModel
+
+MODEL_ID = "suno/bark"
+VOICE_PRESETS = [f"v2/pt_speaker_{index}" for index in range(10)]
+
+RESULTS = Path("results") / "bark"
+
 if __name__ == "__main__":
-    pass
+    ensure_dir(RESULTS)
+
+    for voice_preset in VOICE_PRESETS:
+        processor = AutoProcessor.from_pretrained(MODEL_ID)
+        model = BarkModel.from_pretrained(MODEL_ID)
+
+        inputs = processor("João Palmeiro", voice_preset=voice_preset)
+
+        audio_array = model.generate(**inputs)
+
+        write_wav(
+            RESULTS / f"my_name_{voice_preset.replace('/', '_')}.wav",
+            rate=model.generation_config.sample_rate,
+            data=audio_array.cpu().numpy().squeeze(),
+        )
